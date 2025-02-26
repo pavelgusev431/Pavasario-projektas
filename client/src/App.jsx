a31ffe8321f23348f471285dfd689f4490467696
pavelgusev431 <pavel.gusev@stud.techin.lt>
2025-02-26 13:39:04 +0200

    Added temporary home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router';
+import Home from './components/layout/Home';
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route index element={}></Route>
+          <Route index element={<Home/>}></Route>
         </Routes>
       </Router>
     </>
