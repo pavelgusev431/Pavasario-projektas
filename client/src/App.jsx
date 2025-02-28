db9e5d3c3f2fb88e08afcd12ed9c5cb6c2266f27
Jaronimo1337 <jaroslav.paskel@stud.techin.lt>
2025-02-28 14:10:24 +0200

    Improved carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import Auth from './components/layout/Auth.jsx';
+// import Auth from './components/layout/Auth.jsx';
 import Home from './components/layout/Home.jsx';
-import NotFound from './components/layout/NotFound.jsx';
-import ProtectedRoutes from './components/layout/ProtectedRoutes.jsx';
-
+// import NotFound from './components/layout/NotFound.jsx';
+// import ProtectedRoutes from './components/layout/ProtectedRoutes.jsx';
+import BannerCarousel from './components/Carousel';
 function App() {
     return (
         <>
             <Router>
                 <Routes>
-                    <Route index element={<Auth />} />
-                    <Route path="home" element={<Home />} />
-                    {ProtectedRoutes()}
-                    <Route path="*" element={<NotFound />} />
+                    {/* <Route index element={<Auth />} /> */}
+                    <Route path="/" element={<Home />} />
+                    {/* {ProtectedRoutes()} */}
+                    {/* <Route path="*" element={<NotFound />} /> */}
                 </Routes>
             </Router>
         </>
