72518569a543de6c20436fc4ffa40c7585c37009
KarolisRZN <qewmnbp@gmail.com>
2025-03-19 13:58:11 +0200

    added click on the product

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import Auth from './components/layout/Register_Login/Auth.jsx';
-import Home from './components/layout/Home.jsx';
-import Contact from './components/layout/Contact.jsx';
-import About from './components/layout/About.jsx';
-import NotFound from './components/layout/NotFound.jsx';
-import PasswordReset from './components/layout/Register_Login/PasswordReset.jsx';
-import ProductList from './components/ProductList.jsx';
-import NavBar from './components/layout/NavBar.jsx';
-import UserProducts from './components/UserProducts.jsx';
-function App() {
-    return (
-        <Router>
-            <NavBar />
-            <Routes>
-                <Route index element={<Auth />} />
-                <Route path="home" element={<Home />} />
-                <Route path="products/:id" element={<UserProducts />} />
+import { BrowserRouter as Router, Routes, Route } from "react-router";
+import Auth from "./components/layout/Register_Login/Auth.jsx";
+import Home from "./components/layout/Home.jsx";
+import Contact from "./components/layout/Contact.jsx";
+import About from "./components/layout/About.jsx";
+import NotFound from "./components/layout/NotFound.jsx";
+import PasswordReset from "./components/layout/Register_Login/PasswordReset.jsx";
+import ProductList from "./components/ProductList.jsx";
+import NavBar from "./components/layout/NavBar.jsx";
+import UserProducts from "./components/UserProducts.jsx";
+import ProductDetails from "./components/ProductDetails.jsx"; // Import the new component
 
-                <Route path="contact" element={<Contact />} />
-                <Route path="about" element={<About />} />
-                <Route path="signup" element={<Auth authType="signup" />} />
-                <Route path="reset/:userid/:salt" element={<PasswordReset />} />
-                <Route path="products" element={<ProductList />} />
-                <Route path="*" element={<NotFound />} />
-            </Routes>
-        </Router>
-    );
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route index element={<Auth />} />
+        <Route path="home" element={<Home />} />
+        <Route path="products/:id" element={<UserProducts />} />
+        <Route path="products/selected/:id" element={<ProductDetails />} />{" "}
+        {/* Add the new route */}
+        <Route path="contact" element={<Contact />} />
+        <Route path="about" element={<About />} />
+        <Route path="signup" element={<Auth authType="signup" />} />
+        <Route path="reset/:userid/:salt" element={<PasswordReset />} />
+        <Route path="products" element={<ProductList />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/ProductDetails.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
+
+const ProductDetails = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await axios.get(`/products/selected/${id}`);
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p>Price: ${product.price}</p>
+      <img src={product.image_url} alt={product.name} />
+    </div>
+  );
+};
+
+export default ProductDetails;
