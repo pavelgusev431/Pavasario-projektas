613ca55ae846370b84250a494ce6d4911e58f3d6
KarolisRZN <qewmnbp@gmail.com>
2025-03-25 13:08:36 +0200

    added button buy product

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -62,6 +62,9 @@ const ProductDetails = () => {
           <p className={`text-lg font-bold mb-4 ${stockClass}`}>
             {stockStatus}
           </p>
+          <button className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600">
+            Buy Now
+          </button>
         </div>
       </div>
     </div>
