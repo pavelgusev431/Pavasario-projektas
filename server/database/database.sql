f81069a145fb47dd524211bd6eb4e17bf55e968f
TomasMucunas <tomas.muczunas@gmail.com>
2025-03-31 13:12:04 +0300

    fix data

--- server/database/database.sql
+++ server/database/database.sql
@@ -4,84 +4,48 @@ INSERT INTO "users" (username, email, image_url, description, contacts) VALUES
 ('pavardenis Vardenis', 'pavardenis@gmail.com', 'https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg', 'text text write here more 5 words', 'Konstutucijos 45, Vilnius'),
 ('trending', 'trending@gmail.com', 'https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg', 'text text write here more 5 words',  'Konstutucijos 45, Vilnius'),
 ('bestnew', 'bestnew@gmail.com', 'https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg',  'text text write here more 5 words', 'Konstutucijos 45, Vilnius');
- 
- 
-INSERT INTO products (user_id,category_id, subcategory_id, name, price, description, image_url, amount_in_stock)
+
+
+ALTER TABLE products
+ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+INSERT INTO products (user_id, category_id, subcategory_id, name, price, description, image_url, amount_in_stock, created_at, updated_at)
 VALUES
 -- Pavel Gusev
-<<<<<<< HEAD
-(1, 1, 'iPhone 15 Pro', 129900, 'Naujausias Apple telefonas', 'https://cdn.pixabay.com/photo/2013/07/12/18/39/smartphone-153650_640.png', 0, '2025-01-01 10:00:00', '2025-01-01 10:00:00'),
-(1, 2, 'MacBook Air M2', 239900, 'testas', 'https://techcrunch.com/wp-content/uploads/2022/07/CMC_1580.jpg', 30, '2025-01-05 10:00:00', '2025-01-05 10:00:00'),
-(1, 3, 'AirPods Pro', 79900, 'Aukštos kokybės Apple ausinės', 'https://istore.lt/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/a/i/airpods_4_pdp_image_position_2__wwen.jpg', 40, '2025-01-10 10:00:00', '2025-01-10 10:00:00'),
-(1, 4, 'Apple Watch Series 8', 109900, 'Išmanusis laikrodis su naujomis funkcijomis', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfEI1ZxzdYj2i4XKe7nExt_GipGBGq31mvSw&s', 25, '2025-01-15 10:00:00', '2025-01-15 10:00:00'),
-(1, 5, 'iPad Pro 12.9"', 159900, 'Naujausias Apple planšetinis kompiuteris', 'https://istore.lt/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/i/p/ipad-pro-12.9-wifi-space-gray_9_3.png', 20, '2025-01-20 10:00:00', '2025-01-20 10:00:00'),
-(1, 6, 'Apple TV 4K', 179900, 'Streamingo įrenginys su 4K vaizdo kokybe', 'https://istore.lt/media/catalog/product/a/p/apple-tv-4k-2022_2.png', 15, '2025-01-25 10:00:00', '2025-01-25 10:00:00'),
-(1, 7, 'Beats Studio3 Wireless', 89900, 'Beats ausinės su triukšmo slopinimu', 'https://istore.lt/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/m/q/mq562-beats-studio3-over-ear-headphones-black-1.jpg', 50, '2025-01-30 10:00:00', '2025-01-30 10:00:00'),
-(1, 8, 'Apple Magic Mouse', 69900, 'Ergonomiška Apple pelė', 'https://picfit.topocentras.lt/xcdn-cgi/image/fit=contain,format=auto,quality=85/media/catalog/product/m/m/mmmq3_464582_1647337558.jpg', 30, '2025-02-01 10:00:00', '2025-02-01 10:00:00'),
-(1, 9, 'Apple Keyboard', 99900, 'Elegantiška Apple klaviatūra', 'https://istore.lt/media/catalog/product/m/q/mq052-apple-magic-keyboard-numeric-1.jpg', 40, '2025-02-05 10:00:00', '2025-02-05 10:00:00'),
-(1, 10, 'iPhone 14 Pro', 119900, 'Ankstesnis modelis, vis dar puikus', 'https://alephksa.com/cdn/shop/files/iPhone_14_Pro_Silver_PDP_Image_Position-1a_EN_3f4b45ff-90bc-420b-9235-0a2dfa7d511b.jpg?v=1688732322&width=1445', 60, '2025-02-10 10:00:00', '2025-02-10 10:00:00'),
+(2, 1, 1, 'iPhone 15 Pro', 1299.00, 'Naujausias Apple telefonas', 'https://cdn.pixabay.com/photo/2013/07/12/18/39/smartphone-153650_640.png', 50, '2025-02-28 09:00:00', '2025-03-02 09:00:00'),
+(2, 1, 2, 'MacBook Air M2', 2399.00, 'testas', 'https://techcrunch.com/wp-content/uploads/2022/07/CMC_1580.jpg', 30, '2025-02-28 10:30:00', '2025-03-02 10:30:00'),
+(2, 1, 3, 'AirPods Pro', 799.00, 'Aukštos kokybės Apple ausinės', 'https://istore.lt/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/a/i/airpods_4_pdp_image_position_2__wwen.jpg', 40, '2025-02-28 11:45:00', '2025-03-02 11:45:00'),
+(2, 1, 4, 'Apple Watch Series 8', 1099.00, 'Išmanusis laikrodis su naujomis funkcijomis', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfEI1ZxzdYj2i4XKe7nExt_GipGBGq31mvSw&s', 25, '2025-02-28 12:15:00', '2025-03-02 12:15:00'),
+(2, 1, 2, 'iPad Pro 12.9"', 1599.00, 'Naujausias Apple planšetinis kompiuteris', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-ipad-pro-12-wificell-spacegray-2021?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1674663709258', 20, '2025-02-28 13:30:00', '2025-03-02 13:30:00'),
+(2, 1, 5, 'Apple TV 4K', 1799.00, 'Streamingo įrenginys su 4K vaizdo kokybe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQITvFPp3DQ4ngX545DXjxiIyAeRB0sHptwDQ&s', 15, '2025-02-28 14:00:00', '2025-03-02 14:00:00'),
+(2, 1, 3, 'Beats Studio3 Wireless', 899.00, 'Beats ausinės su triukšmo slopinimu', 'https://istore.lt/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/m/q/mqtp3-beats-pro-black-4.jpeg', 50, '2025-02-28 15:15:00', '2025-03-02 15:15:00'),
+(2, 1, 1, 'Apple Magic Mouse', 699.00, 'Ergonomiška Apple pelė', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MXK63?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1730508287136', 30, '2025-02-28 16:30:00', '2025-03-02 16:30:00'),
+(2, 1, 1, 'Apple Keyboard', 999.00, 'Elegantiška Apple klaviatūra', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRdHj0aRrA7hlxfiXu8SQEuNLxWyxed2i_Vg&s', 40, '2025-02-28 17:45:00', '2025-03-02 17:45:00'),
+(2, 1, 1, 'iPhone 14 Pro', 1199.00, 'Ankstesnis modelis, vis dar puikus', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW8lvCIRMAbJ0RuaeQP74f90i8dQR-IMqXDA&s', 60, '2025-02-28 18:00:00', '2025-03-02 18:00:00'),
 
 -- Vardenis Vardenis
-(3, 1, 'Samsung Galaxy S24', 999.00, 'Galingas Samsung flagmanas', 'https://kainos-img.dgn.lt/photos2_25_268648490/img.jpg', 30, '2025-02-15 10:00:00', '2025-02-15 10:00:00'),
-(3, 2, 'Samsung Galaxy Tab S8', 849.00, 'Samsung planšetinis kompiuteris', 'https://www.varle.lt/static/uploads/products/554/sam/samsung-galaxy-tab-s8-5g-plansete-8gb--32b4543014.png', 50, '2025-02-20 10:00:00', '2025-02-20 10:00:00'),
-(3, 3, 'Samsung Galaxy Buds2', 399.00, 'Komfortiškos ausinės su aktyviu triukšmo slopinimu', 'https://www.varle.lt/static/uploads/products/886/aus/ausines-sm-r177nzt-samsung-galaxy-buds-2.jpg', 40, '2025-02-25 10:00:00', '2025-02-25 10:00:00'),
-(3, 4, 'Samsung SmartWatch', 799.00, 'Išmanusis laikrodis su nuotolinio valdymo funkcija', 'https://www.irisimo.lt/files/product/56236/SAMSUNG-Galaxy-Watch-6-44mm-Grafite-SM-R940NZKAEUE.gif', 35, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(3, 5, 'Samsung QLED TV', 1499.00, 'Aukštos kokybės televizorius su QLED technologija', 'https://images.samsung.com/is/image/samsung/p6pim/ca/qn55q70dafxzc/gallery/ca-qled-q70d-qn55q70dafxzc-540022102?$684_547_PNG$', 45, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(3, 6, 'Samsung Notebook', 1099.00, 'Galingas nešiojamas kompiuteris', 'https://images.samsung.com/is/image/samsung/assets/us/galaxybooks/01182025/GB-PCD-Discover-FT12-FeatureColumnCarousel-GB5Pro360-M-V3.jpg?$720_N_JPG$', 40, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(3, 7, 'Samsung Soundbar', 799.00, 'Garsinė juosta su aukšta garso kokybe', 'https://image-us.samsung.com/SamsungUS/home/television-home-theater/home-theater/soundbars/02152024/HW-Q990D_002_Set-R_Perspective_Graphite_Black_1600x1200.jpg?$product-details-jpg$', 60, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(3, 8, 'Samsung Monitor', 599.00, 'Didelis ekranas su 4K raiška', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Xk9-82MPIO_iMgGBjAmk09WWWFXeqZxFqQ&s', 50, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(3, 9, 'Samsung Powerbank', 249.00, 'Nešiojamas įkroviklis', 'https://www.varle.lt/static/uploads/products/496/sam/samsung-eb-u3300xjegeu-nesiojamasis-ikroviklis_UgzXRJC.jpg', 70, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(3, 10, 'Samsung External SSD', 459.00, 'Greitas išorinis SSD diskas', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU7DWLxqQhqoAicUNO0RWMdCuOSvP2OVrplA&s', 30, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
+(3, 1, 1, 'Samsung Galaxy S24', 999.00, 'Galingas Samsung flagmanas', 'https://kainos-img.dgn.lt/photos2_25_268648490/img.jpg', 30, '2025-03-01 09:00:00', '2025-03-03 09:00:00'),
+(3, 1, 1, 'Samsung Galaxy Tab S8', 849.00, 'Samsung planšetinis kompiuteris', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoK3wn3Ep9Yx3evtt8QWedwsnH8vRTo3dsmA&s', 50, '2025-03-01 10:30:00', '2025-03-03 10:30:00'),
+(3, 1, 3, 'Samsung Galaxy Buds2', 399.00, 'Komfortiškos ausinės su aktyviu triukšmo slopinimu', 'https://static1.xdaimages.com/wordpress/wp-content/uploads/2022/11/Samsung-Galaxy-Buds-2-Pro-in-Bora-Purple-colorway.jpeg', 40, '2025-03-01 11:45:00', '2025-03-03 11:45:00'),
+(3, 1, 4, 'Samsung SmartWatch', 799.00, 'Išmanusis laikrodis su nuotolinio valdymo funkcija', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTub11bCaWROg6b5mQZi4N07UIjp4ysfTHflQ&s', 35, '2025-03-01 12:15:00', '2025-03-03 12:15:00'),
+(3, 1, 5, 'Samsung QLED TV', 1499.00, 'Aukštos kokybės televizorius su QLED technologija', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxdWRF198EPZdgUmuQ1R0aI136jgMZ0x7xmA&s', 45, '2025-03-01 13:30:00', '2025-03-03 13:30:00'),
+(3, 1, 2, 'Samsung Notebook', 1099.00, 'Galingas nešiojamas kompiuteris', 'https://preview.redd.it/screen-size-comparison-between-2022-14-6-samsung-galaxy-tab-v0-pzbjjkac5h4c1.jpg?width=640&crop=smart&auto=webp&s=cffae25cba9d6756f3b5d21db3d38c69189e5ebb', 40, '2025-03-01 14:45:00', '2025-03-03 14:45:00'),
+(3, 1, 3, 'Samsung Soundbar', 799.00, 'Garsinė juosta su aukšta garso kokybe', 'https://www.telia.lt/medias/garso-sistema-lg-s95tr.jpg?context=bWFzdGVyfHJvb3R8MTIyMjR8aW1hZ2UvanBlZ3xoYjIvaDg4LzEzMjg0MDc5OTYwMDk0LmpwZ3w5ZGYyZjkxZjRhOWMxNzQ2NzFiZDExMTQwNGViY2FkZGY0NjdmYzkzYmI0ZmNlNjFhZmM1OWQ3ZjJlYTcwZjdl', 60, '2025-03-01 15:15:00', '2025-03-03 15:15:00'),
+(3, 1, 2, 'Samsung Monitor', 599.00, 'Didelis ekranas su 4K raiška', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Xk9-82MPIO_iMgGBjAmk09WWWFXeqZxFqQ&s', 50, '2025-03-01 16:00:00', '2025-03-03 16:00:00'),
+(3, 1, 1, 'Samsung Powerbank', 249.00, 'Nešiojamas įkroviklis', 'https://www.varle.lt/static/uploads/products/496/sam/samsung-eb-u3300xjegeu-nesiojamasis-ikroviklis_UgzXRJC.jpg', 70, '2025-03-01 17:00:00', '2025-03-03 17:00:00'),
+(3, 1, 1, 'Samsung External SSD', 459.00, 'Greitas išorinis SSD diskas', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU7DWLxqQhqoAicUNO0RWMdCuOSvP2OVrplA&s', 30, '2025-03-01 18:00:00', '2025-03-03 18:00:00'),
 
 -- testas
-(4, 2, 'MacBook Air M2', 2399.00, 'testas', 'https://techcrunch.com/wp-content/uploads/2022/07/CMC_1580.jpg', 30, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
+(4, 1, 2, 'MacBook Air M2', 2399.00, 'testas', 'https://bgr.com/wp-content/uploads/2022/06/MacBook-Air-M2.jpg?quality=82&strip=all&w=1020&h=574&crop=1', 30, '2025-03-02 09:00:00', '2025-03-04 09:00:00'),
+(5, 1, 5, 'betkas', 15, 'testuoju', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5BiA2UWH55YtwpfFZHQE_y6t45_x8BttMCA&s', 20, '2025-03-02 10:00:00', '2025-03-04 10:00:00'),
+(5, 1, 1, 'Testo telefonas', 399.99, 'Tai tik testinis telefonas', 'https://picfit.topocentras.lt/xcdn-cgi/image/fit=contain,format=auto,width=300,quality=85/media/catalog/product/f/i/file_720555_1737852227.jpg', 10, '2025-03-02 11:00:00', '2025-03-04 11:00:00'),
+(5, 1, 2, 'Testo kompiuteris', 499.99, 'Testo kompiuteris su labai gera kaina', 'https://ai-techreport.com/wp-content/uploads/2024/07/converted_image-39-1.webp', 5, '2025-03-02 12:00:00', '2025-03-04 12:00:00'),
+(5, 1, 3, 'Testo ausinės', 199.99, 'Testinės ausinės', 'https://priedai.lt/e/media/catalog/product/cache/1/small_image/252x252/9df78eab33525d08d6e5fb8d27136e95/a/p/apple-earpods-originaiosl-iphone-ipad-ipod-ausines.jpg', 15, '2025-03-02 13:00:00', '2025-03-04 13:00:00'),
+(5, 1, 4, 'Testo laikrodis', 249.99, 'Testinis laikrodis su visomis funkcijomis', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkACTKQ8DQXViZSKoBQe0aDFrGfrnc34H1ww&s', 8, '2025-03-02 14:00:00', '2025-03-04 14:00:00'),
+(5, 1, 5, 'Tikrinu ar veikia data', 249, 'tikrinu', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYlODcqSHiGaEh6eLqV-nH8wxlHLoMPsP8eg&s', 8, '2025-03-02 15:00:00', '2025-03-04 15:00:00');
 
--- testas
-(5, 5, 'betkas', 15, 'testuoju', 'https://martech.org/wp-content/uploads/2014/08/facebook-like-button-1920.jpg', 20, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-
-(5, 1, 'Testo telefonas', 399.99, 'Tai tik testinis telefonas', 'https://media.bite.lt/@bite-lt/sites/default/files/2024-07/koks_telefonas_jums_tinkamiausia.jpg', 10, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(5, 2, 'Testo kompiuteris', 499.99, 'Testo kompiuteris su labai gera kaina', 'https://topcom.lt/wp-content/uploads/2015/11/kompiuteris.jpg', 5, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(5, 3, 'Testo ausinės', 199.99, 'Testinės ausinės', 'https://static.testo.com/image/upload/c_pad,h_500,w_775,f_auto,q_auto/HQ/bild1-testo-sensor-ld-trans-2000x1500-free.jpg', 15, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(5, 4, 'Testo laikrodis', 249.99, 'Testinis laikrodis su visomis funkcijomis', 'https://media.bite.lt/@bite-lt/sites/default/files/2024-07/koks_ismanusis_laikrodis_jums_ti.jpg', 8, '2025-02-28 10:00:00', '2025-02-28 10:00:00'),
-(5, 4, 'Tikrinu ar veikia data', 249, 'tikrinu', 'https://static6.depositphotos.com/1005979/599/i/950/depositphotos_5999249-stock-photo-test-day-circled-on-calendar.jpg', 8, '2025-02-28 10:00:00', '2025-02-28 10:00:00');
-
-
-=======
-(2, 1,1, 'iPhone 15 Pro', 1299.00, 'Naujausias Apple telefonas', 'https://cdn.pixabay.com/photo/2013/07/12/18/39/smartphone-153650_640.png', 50),
-(2, 1,2, 'MacBook Air M2', 2399.00, 'testas', 'https://techcrunch.com/wp-content/uploads/2022/07/CMC_1580.jpg', 30),
-(2, 1,3, 'AirPods Pro', 799.00, 'Aukštos kokybės Apple ausinės', 'https://istore.lt/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/a/i/airpods_4_pdp_image_position_2__wwen.jpg', 40),
-(2, 1,4, 'Apple Watch Series 8', 1099.00, 'Išmanusis laikrodis su naujomis funkcijomis', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfEI1ZxzdYj2i4XKe7nExt_GipGBGq31mvSw&s', 25),
-(2, 1,2, 'iPad Pro 12.9"', 1599.00, 'Naujausias Apple planšetinis kompiuteris', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/refurb-ipad-pro-12-wificell-spacegray-2021?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1674663709258', 20),
-(2, 1,5, 'Apple TV 4K', 1799.00, 'Streamingo įrenginys su 4K vaizdo kokybe', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQITvFPp3DQ4ngX545DXjxiIyAeRB0sHptwDQ&s', 15),
-(2, 1,3, 'Beats Studio3 Wireless', 899.00, 'Beats ausinės su triukšmo slopinimu', 'https://istore.lt/media/catalog/product/cache/1/image/800x/602f0fa2c1f0d1ba5e241f914e856ff9/m/q/mqtp3-beats-pro-black-4.jpeg', 50),
-(2, 1,1, 'Apple Magic Mouse', 699.00, 'Ergonomiška Apple pelė', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MXK63?wid=2000&hei=2000&fmt=jpeg&qlt=95&.v=1730508287136', 30),
-(2, 1,1, 'Apple Keyboard', 999.00, 'Elegantiška Apple klaviatūra', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRdHj0aRrA7hlxfiXu8SQEuNLxWyxed2i_Vg&s', 40),
-(2, 1,1, 'iPhone 14 Pro', 1199.00, 'Ankstesnis modelis, vis dar puikus', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW8lvCIRMAbJ0RuaeQP74f90i8dQR-IMqXDA&s', 60),
  
--- Vardenis Vardenis
-(3, 1,1, 'Samsung Galaxy S24', 999.00, 'Galingas Samsung flagmanas', 'https://kainos-img.dgn.lt/photos2_25_268648490/img.jpg', 30),
-(3, 1,1, 'Samsung Galaxy Tab S8', 849.00, 'Samsung planšetinis kompiuteris', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoK3wn3Ep9Yx3evtt8QWedwsnH8vRTo3dsmA&s', 50),
-(3, 1,3, 'Samsung Galaxy Buds2', 399.00, 'Komfortiškos ausinės su aktyviu triukšmo slopinimu', 'https://static1.xdaimages.com/wordpress/wp-content/uploads/2022/11/Samsung-Galaxy-Buds-2-Pro-in-Bora-Purple-colorway.jpeg', 40),
-(3, 1,4, 'Samsung SmartWatch', 799.00, 'Išmanusis laikrodis su nuotolinio valdymo funkcija', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTub11bCaWROg6b5mQZi4N07UIjp4ysfTHflQ&s', 35),
-(3, 1,5, 'Samsung QLED TV', 1499.00, 'Aukštos kokybės televizorius su QLED technologija', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxdWRF198EPZdgUmuQ1R0aI136jgMZ0x7xmA&s', 45),
-(3, 1,2, 'Samsung Notebook', 1099.00, 'Galingas nešiojamas kompiuteris', 'https://preview.redd.it/screen-size-comparison-between-2022-14-6-samsung-galaxy-tab-v0-pzbjjkac5h4c1.jpg?width=640&crop=smart&auto=webp&s=cffae25cba9d6756f3b5d21db3d38c69189e5ebb', 40),
-(3, 1,3, 'Samsung Soundbar', 799.00, 'Garsinė juosta su aukšta garso kokybe', 'https://www.telia.lt/medias/garso-sistema-lg-s95tr.jpg?context=bWFzdGVyfHJvb3R8MTIyMjR8aW1hZ2UvanBlZ3xoYjIvaDg4LzEzMjg0MDc5OTYwMDk0LmpwZ3w5ZGYyZjkxZjRhOWMxNzQ2NzFiZDExMTQwNGViY2FkZGY0NjdmYzkzYmI0ZmNlNjFhZmM1OWQ3ZjJlYTcwZjdl', 60),
-(3, 1,2, 'Samsung Monitor', 599.00, 'Didelis ekranas su 4K raiška', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Xk9-82MPIO_iMgGBjAmk09WWWFXeqZxFqQ&s', 50),
-(3, 1,1, 'Samsung Powerbank', 249.00, 'Nešiojamas įkroviklis', 'https://www.varle.lt/static/uploads/products/496/sam/samsung-eb-u3300xjegeu-nesiojamasis-ikroviklis_UgzXRJC.jpg', 70),
-(3, 1,1, 'Samsung External SSD', 459.00, 'Greitas išorinis SSD diskas', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU7DWLxqQhqoAicUNO0RWMdCuOSvP2OVrplA&s', 30),
- 
--- testas
-(4, 1,2, 'MacBook Air M2', 2399.00, 'testas', 'https://bgr.com/wp-content/uploads/2022/06/MacBook-Air-M2.jpg?quality=82&strip=all&w=1020&h=574&crop=1', 30),
---testas
-(5, 1,5, 'betkas', 15, 'testuoju', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5BiA2UWH55YtwpfFZHQE_y6t45_x8BttMCA&s', 20),
- 
-(5, 1,1, 'Testo telefonas', 399.99, 'Tai tik testinis telefonas', 'https://picfit.topocentras.lt/xcdn-cgi/image/fit=contain,format=auto,width=300,quality=85/media/catalog/product/f/i/file_720555_1737852227.jpg', 10),
-(5, 1,2, 'Testo kompiuteris', 499.99, 'Testo kompiuteris su labai gera kaina', 'https://ai-techreport.com/wp-content/uploads/2024/07/converted_image-39-1.webp', 5),
-(5, 1,3, 'Testo ausinės', 199.99, 'Testinės ausinės', 'https://priedai.lt/e/media/catalog/product/cache/1/small_image/252x252/9df78eab33525d08d6e5fb8d27136e95/a/p/apple-earpods-originaiosl-iphone-ipad-ipod-ausines.jpg', 15),
-(5, 1,4, 'Testo laikrodis', 249.99, 'Testinis laikrodis su visomis funkcijomis', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkACTKQ8DQXViZSKoBQe0aDFrGfrnc34H1ww&s', 8),
-(5, 1,5, 'Tikrinu ar veikia data', 249, 'tikrinu', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYlODcqSHiGaEh6eLqV-nH8wxlHLoMPsP8eg&s', 8);
- 
->>>>>>> 518e67b4b5934fb0de304ad4f37c01d48b35a941
 INSERT INTO "events" (user_id,product_id, type_id, target_id,  description, timestamp)
 VALUES
 -- userio sukurimas
