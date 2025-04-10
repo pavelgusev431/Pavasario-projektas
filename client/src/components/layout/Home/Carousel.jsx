974846c2976a6174e5f3397b00b4f776d2ca81c5
KarolisRZN <qewmnbp@gmail.com>
2025-04-10 11:22:35 +0300

    carousel

--- client/src/components/layout/Home/Carousel.jsx
+++ client/src/components/layout/Home/Carousel.jsx
@@ -26,25 +26,51 @@ const BannerCarousel = () => {
     };
 
     return (
-        <Carousel
-            showThumbs={false}
-            infiniteLoop
-            autoPlay
-            stopOnHover
-            swipeable
-            emulateTouch
-            showStatus={false}
-        >
-            {images.map((image, index) => (
-                <div
-                    key={index}
-                    onClick={() => handleImageClick(image.link)}
-                    className="cursor-pointer"
-                >
-                    <img src={image.src} alt={`Slide ${index + 1}`} />
-                </div>
-            ))}
-        </Carousel>
+        <div className="relative">
+            <Carousel
+                showThumbs={false}
+                infiniteLoop
+                autoPlay
+                stopOnHover
+                swipeable
+                emulateTouch
+                showStatus={false}
+                renderArrowPrev={(onClickHandler, hasPrev, label) =>
+                    hasPrev && (
+                        <button
+                            type="button"
+                            onClick={onClickHandler}
+                            className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white text-black rounded-full w-14 h-14 flex items-center justify-center hover:bg-gray-300 hover:text-black cursor-pointer z-10"
+                            aria-label={label}
+                        >
+                            &#8249; {/* Left arrow */}
+                        </button>
+                    )
+                }
+                renderArrowNext={(onClickHandler, hasNext, label) =>
+                    hasNext && (
+                        <button
+                            type="button"
+                            onClick={onClickHandler}
+                            className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white text-black rounded-full w-14 h-14 flex items-center justify-center hover:bg-gray-300 hover:text-black cursor-pointer z-10"
+                            aria-label={label}
+                        >
+                            &#8250; {/* Right arrow */}
+                        </button>
+                    )
+                }
+            >
+                {images.map((image, index) => (
+                    <div
+                        key={index}
+                        onClick={() => handleImageClick(image.link)}
+                        className="cursor-pointer"
+                    >
+                        <img src={image.src} alt={`Slide ${index + 1}`} />
+                    </div>
+                ))}
+            </Carousel>
+        </div>
     );
 };
 
