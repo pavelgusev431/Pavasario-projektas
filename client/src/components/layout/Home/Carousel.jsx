bd4049f5f8d09ff02be497428b69faa105fd6958
KarolisRZN <qewmnbp@gmail.com>
2025-04-10 11:18:48 +0300

    carousel routes add

--- client/src/components/layout/Home/Carousel.jsx
+++ client/src/components/layout/Home/Carousel.jsx
@@ -2,13 +2,29 @@ import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const images = [
-    '../src/public/banner_images/1.png',
-    '../src/public/banner_images/2.png',
-    '../src/public/banner_images/3.png',
-    '../src/public/banner_images/4.png',
+    {
+        src: '../src/public/banner_images/1.png',
+        link: 'http://localhost:5173/products/s/1',
+    },
+    {
+        src: '../src/public/banner_images/2.png',
+        link: 'http://localhost:5173/products/s/6',
+    },
+    {
+        src: '../src/public/banner_images/3.png',
+        link: 'http://localhost:5173/products/s/2',
+    },
+    {
+        src: '../src/public/banner_images/4.png',
+        link: 'http://localhost:5173/products/s/16',
+    },
 ];
 
 const BannerCarousel = () => {
+    const handleImageClick = (link) => {
+        window.location.href = link;
+    };
+
     return (
         <Carousel
             showThumbs={false}
@@ -19,9 +35,13 @@ const BannerCarousel = () => {
             emulateTouch
             showStatus={false}
         >
-            {images.map((src, index) => (
-                <div key={index}>
-                    <img src={src} alt={`Slide ${index + 1}`} />
+            {images.map((image, index) => (
+                <div
+                    key={index}
+                    onClick={() => handleImageClick(image.link)}
+                    className="cursor-pointer"
+                >
+                    <img src={image.src} alt={`Slide ${index + 1}`} />
                 </div>
             ))}
         </Carousel>
