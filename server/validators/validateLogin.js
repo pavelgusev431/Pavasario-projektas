90d30745515c46c05b5c3a6a8850510ea4f7d4dc
Darius911 <darius.alksnis@gmail.com>
2025-04-30 13:16:36 +0300

    remove comment

--- server/validators/validateLogin.js
+++ server/validators/validateLogin.js
@@ -23,17 +23,17 @@ const validateLogin = [
             }
         }),
 
-    // body('password')
-    //     .trim()
-    //     .escape()
-    //     .notEmpty()
-    //     .withMessage('Password hash is a required field')
-    //     .isString()
-    //     .withMessage('Password hash must be a string')
-    //     .matches(/^[a-z\d]{64}$/)
-    //     .withMessage('Password hash must be a valid hash')
-    //     .isLength({ min: 64, max: 64 })
-    //     .withMessage('Password hash must be 64 characters long'),
+    body('password')
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage('Password hash is a required field')
+        .isString()
+        .withMessage('Password hash must be a string')
+        .matches(/^[a-z\d]{64}$/)
+        .withMessage('Password hash must be a valid hash')
+        .isLength({ min: 64, max: 64 })
+        .withMessage('Password hash must be 64 characters long'),
 ];
 
 export default validateLogin;
